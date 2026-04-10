a287aa52155f0871b1adf8851f143c1a5b75d8f5
Afisty Nayyara <afistynayyara@gmail.com>
2026-04-10 13:09:19 +0700

    balikin P2

---
+++ P2_Nayyara/src/p2_nayyara/P2_Nayyara.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package p2_nayyara;
+
+/**
+ *
+ * @author Asus
+ */
+public class P2_Nayyara {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
